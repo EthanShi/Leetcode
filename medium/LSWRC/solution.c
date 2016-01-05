fcf4d0eb2b0b027ed9faed3abd1892183cf5b599
aline <aline@localhost.localdomain>
2016-01-05 21:42:31 +0800

    optimized the algorithm of LSWRC, now it can beat 97.8% on LeetCode.

--- medium/LSWRC/solution.c
+++ medium/LSWRC/solution.c
@@ -1,6 +1,6 @@
 /*
  * Author: Ethan Shi
- * Version: V1.0.0
+ * Version: V1.1.0
  * Date: 2016-1-4
  * Description:
  *   this is a solution of 
@@ -9,6 +9,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	char* str = "abcddddaacds";
@@ -18,48 +19,27 @@ int main() {
 
 /*find the longest sub string without repeating of input*/
 int lengthOfLongestSubstring(char* s) {
-    char* in = s;
     int index[127];		/*record a char's index in the input string*/
-    int used[127];		/*record used indexs of index[]*/
-    int uc = 0;			/*used count*/
+	int start = 0;
+	int end = 0;
     int count = 0;		/*count of without repeating characters*/
     int max = 0;		/*max of all counts*/
-    int i;
-    for(i=0; i<127; i++) {		/*initalize the array*/
-        index[i] = -1;
-        used[i] = -1;
-    }
-    i=0;
-    while(in[i] != '\0') {		/*read inputs*/
-        char c = in[i];
-        if(index[c] == -1) {	/*no repeat*/
-            index[c] = i;
+    memset(index, -1, sizeof(index));		/*initalize the array*/
+    while(s[end] != '\0') {		/*read inputs*/
+        char c = s[end];
+        if(index[c] < start) {	/*no repeat*/
             count++;
-            used[uc] = c;
-            uc++;
         }
         else {					/*repeated*/
-            int j = 0;
-            int rpi = index[c]; /*repeated character's index*/
             if(count > max) {	/*save the max length*/
                 max = count;
             }
-            while(used[j] != -1) {			/*clear index[] accroding to used[]*/
-                int idx = used[j];
-                if(index[idx] < rpi) {		/*only clear characters before rpi*/
-                    index[idx] = -1;
-                    uc--;
-                    used[j] = used[uc];		/*concentrate item of used[]*/
-                    used[uc] = -1;
-                    count--;				/*adjust count*/
-                } else {
-                    j++;
-                }
-            }
-            index[c] = i;
+            count = start+count-index[c];
+			start = index[c]+1;
         }
-        i++;
-    }
+        index[c] = end;
+		end++;
+	}
     if(count > max) {						/*consider the langest substring is at last*/
         max = count;
     }
